e08b28bb9f5541ffa5a896483f4f7d6dc1bdef55
Rodrigo Bondoc <rbondoc96@gmail.com>
2024-03-30 03:54:34 -0700

    update(www): add missing no-transition CSS class

--- apps/www/src/utilities/theme.ts
+++ apps/www/src/utilities/theme.ts
@@ -11,6 +11,8 @@ export function applyAppTheme(appTheme: AppTheme) {
         ? getSystemTheme()
         : appTheme;
 
+    // Toggling the `no-transition` class defined in `index.scss` makes it so that
+    // any of its descendants won't animate when the `dark` class is added or removed.
     document.body.classList.add('no-transition');
     document.documentElement.classList.toggle('dark', targetTheme === AppTheme.Dark);
     setTimeout(() => document.body.classList.remove('no-transition'), 100);

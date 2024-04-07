ce5f1310d69ba75ae348d94ef8c24ec995d5e900
Rodrigo Bondoc <rbondoc96@gmail.com>
2024-04-06 20:27:58 -0700

    update(www): add Umami analytics only in prod build

--- apps/www/vite.config.ts
+++ apps/www/vite.config.ts
@@ -1,7 +1,26 @@
 import {join, resolve} from 'node:path';
 import {TanStackRouterVite} from '@tanstack/router-vite-plugin';
 import react from '@vitejs/plugin-react-swc';
-import {defineConfig} from 'vite';
+import {defineConfig, type PluginOption} from 'vite';
+
+const transformHtmlPlugin: () => PluginOption = () => {
+    return {
+        name: 'custom-plugin',
+        transformIndexHtml: (html) => {
+            if (process.env.NODE_ENV !== 'production') {
+                return html.replace(
+                    /<!-- Umami Analytics -->/,
+                    '',
+                );
+            }
+
+            return html.replace(
+                /<!-- Umami Analytics -->/,
+                '<script defer src="https://analytics.us.umami.is/script.js" data-website-id="14eea165-166b-4ea8-9350-b2252b37a723"></script>',
+            );
+        },
+    };
+};
 
 const cwd = (...paths: string[]) => join(resolve(__dirname), ...paths);
 
@@ -13,6 +32,7 @@ export default defineConfig({
             quoteStyle: 'single',
             routesDirectory: cwd('src', 'routes'),
         }),
+        transformHtmlPlugin(),
     ],
     resolve: {
         alias: {

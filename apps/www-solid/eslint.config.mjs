f6662f01a9595b02242e8041aa97bea3140112ad
Rodrigo Bondoc <rbondoc96@gmail.com>
2024-04-08 21:45:31 -0700

    build(www-solid): replace biomejs with ESLint

---
+++ apps/www-solid/eslint.config.mjs
@@ -0,0 +1,91 @@
+import eslintJs from '@eslint/js';
+import stylistic from '@stylistic/eslint-plugin';
+import typescript from '@typescript-eslint/eslint-plugin';
+import typescriptParser from '@typescript-eslint/parser';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
+import solid from 'eslint-plugin-solid';
+import globals from 'globals';
+
+/** @type {import('eslint').Linter.FlatConfig[]} */
+const config = [
+    {
+        files: [
+            '**/*.{ts,tsx}',
+        ],
+        ignores: [
+            'dist/**',
+            'node_modules/**',
+        ],
+        languageOptions: {
+            ecmaVersion: 'latest',
+            globals: {
+                ...globals.browser,
+            },
+            parser: typescriptParser,
+            parserOptions: {
+                ecmaFeatures: {
+                    jsx: true,
+                },
+                project: './tsconfig.json',
+            },
+            sourceType: 'module',
+        },
+        linterOptions: {
+            reportUnusedDisableDirectives: true,
+        },
+        plugins: {
+            '@typescript-eslint': typescript,
+            'jsx-a11y': jsxA11y,
+        },
+        rules: {
+            ...eslintJs.configs.recommended.rules,
+            ...typescript.configs.recommended.rules,
+            ...jsxA11y.configs.recommended.rules,
+            'no-redeclare': 'off',
+            'no-unused-vars': 'off',
+        },
+    },
+    {
+        files: [
+            'vite.config.ts',
+            'vitest.config.ts',
+        ],
+        ignores: [
+            'dist/**',
+            'node_modules/**',
+        ],
+        rules: {
+            'no-undef': 'off',
+        },
+    },
+    {
+        ignores: [
+            'dist/**',
+            'node_modules/**',
+        ],
+        plugins: {
+            '@stylistic': stylistic,
+        },
+        rules: {
+            '@stylistic/comma-dangle': ['error', 'always-multiline'],
+            // The number attached to `SwitchCase` actually acts like an indentation multiplier.
+            '@stylistic/indent': ['error', 4, {SwitchCase: 1}],
+            '@stylistic/quotes': ['error', 'single'],
+            '@stylistic/semi': ['error', 'always'],
+        },
+    },
+    {
+        ignores: [
+            'dist/**',
+            'node_modules/**',
+        ],
+        plugins: {
+            solid: solid,
+        },
+        rules: {
+            ...solid.configs.recommended.rules,
+        },
+    },
+];
+
+export default config;

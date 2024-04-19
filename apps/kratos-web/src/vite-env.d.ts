e833a8cfd7280dbbe38b37eee7e4c186bc383a5e
Rodrigo Bondoc <rbondoc96@gmail.com>
2024-04-19 14:27:34 -0700

    refactor(kratos-web): consolidate vite-env and env declarations

--- apps/kratos-web/src/vite-env.d.ts
+++ apps/kratos-web/src/vite-env.d.ts
@@ -1,7 +1,10 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
+    readonly DEV: boolean;
     readonly MODE: 'development' | 'production';
+    readonly PROD: boolean;
+    readonly VITE_BASE_URL: string;
 }
 
 interface ImportMeta {

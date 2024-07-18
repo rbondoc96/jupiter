ca8aaa24674770e95d6732df2c8081c6714e6d1e
Rodrigo Bondoc <rbondoc96@gmail.com>
2024-07-18 02:42:03 -0700

    build(www): revert to only building www when certain dirs change

--- scripts/deploy_www.mjs
+++ scripts/deploy_www.mjs
@@ -3,6 +3,8 @@
 import { rimraf } from 'rimraf';
 
 async function main() {
+    // This script attempts to work around this issue:
+    // https://github.com/npm/cli/issues/4828
     await rimraf('package-lock.json');
     await rimraf('node_modules');
 }
